78c535a3a4f745d5b790320ab489d7a11b906f0c
tuanhiep225 <tuanhiep225@gmail.com>
2019-01-18 18:13:29 +0700

    sua file config

--- src/main/java/com/springmvc/springmongodbweb/SpringMongodbWebApplication.java
+++ src/main/java/com/springmvc/springmongodbweb/SpringMongodbWebApplication.java
@@ -1,11 +1,14 @@
 package com.springmvc.springmongodbweb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories("com.springmvc.springmongodbweb.repositories")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringMongodbWebApplication {
 
 	public static void main(String[] args) {
